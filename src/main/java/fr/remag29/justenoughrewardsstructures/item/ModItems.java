48717b2af90463104d87cc3f5dc8abf74e32a5e1
Remag29 <73289897+Remag29@users.noreply.github.com>
2023-02-22 16:04:48 +0100

    Added Argon Crystal item

--- src/main/java/fr/remag29/justenoughrewardsstructures/item/ModItems.java
+++ src/main/java/fr/remag29/justenoughrewardsstructures/item/ModItems.java
@@ -15,6 +15,7 @@ public class ModItems {
     public static final RegistryObject<Item> COIN_PURSE = ITEMS.register("coin_purse", () -> new Item(new Item.Properties().stacksTo(1)));
     public static final RegistryObject<Item> ANCIENT_EMERALD = ITEMS.register("ancient_emerald", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> ANCIENT_RUBY = ITEMS.register("ancient_ruby", () -> new Item(new Item.Properties()));
+    public static final RegistryObject<Item> ARGON_CRYSTAL = ITEMS.register("argon_crystal", () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
