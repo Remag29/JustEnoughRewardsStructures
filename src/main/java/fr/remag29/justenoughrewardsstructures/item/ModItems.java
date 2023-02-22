080fc85a28e23416f78f759067bfec151499d0af
Remag29 <remag29@outlook.com>
2023-02-22 15:39:56 +0100

    Added Ancient Emerald item

--- src/main/java/fr/remag29/justenoughrewardsstructures/item/ModItems.java
+++ src/main/java/fr/remag29/justenoughrewardsstructures/item/ModItems.java
@@ -13,6 +13,7 @@ public class ModItems {
 
     public static final RegistryObject<Item> TREASURE_COINS = ITEMS.register("treasure_coins", () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> COIN_PURSE = ITEMS.register("coin_purse", () -> new Item(new Item.Properties().stacksTo(1)));
+    public static final RegistryObject<Item> ANCIENT_EMERALD = ITEMS.register("ancient_emerald", () -> new Item(new Item.Properties()));
 
     public static void register(IEventBus eventBus) {
         ITEMS.register(eventBus);
